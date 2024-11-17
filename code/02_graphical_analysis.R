6a82a9719767460a362d22d1428a0e7b723114a0
Linlin Wu <142555452+wull1009@users.noreply.github.com>
2024-11-17 15:46:24 -0500

    Update 02_graphical_analysis.R

--- code/02_graphical_analysis.R
+++ code/02_graphical_analysis.R
@@ -1,11 +1,45 @@
+library(here)
 here::i_am("02_graphical_analysis.R")
 
+data=read.csv(file=here::here(“data/all_data.csv”))
+
 library(ggplot2)
-fig3=ggplot(data=xy,aes(x=children,fill=sex))+geom_histogram(alpha=0.8,position = "dodge",bins = 10)+ggtitle("Fig.3 Distribution plot of the number of children by gender.")+theme(plot.title = element_text(hjust = 0.5))+ 
-  labs(x = "Number of children/dependents", y = "Count")
+
+fig1=ggplot(data, aes(x = age, color = factor(sex), fill = factor(sex))) +
+  geom_density(alpha = 0.4) +
+  labs(x = "Age", y = "Density", title = "Age Density Distribution by Sex") +
+  theme_minimal() +
+  scale_fill_manual(values = c("blue", "pink")) +  
+  scale_color_manual(values = c("blue", "pink"))   
+
+ggplot2::ggsave(
+  fig1, 
+  file = here::here("output/graphical_analysis_fig1.png")
+)
+
+fig2=ggplot(data, aes(x = factor(classification), y = age, fill = factor(sex))) +
+  geom_boxplot() +
+  labs(x = "Classification", y = "Age", 
+       title = "Age Distribution by Classification and Sex") +
+  theme_minimal()
+
+ggplot2::ggsave(
+  fig2, 
+  file = here::here("output/graphical_analysis_fig2.png")
+)
+
+fig3=ggplot(data, aes(x = CLASIFICATION_FINAL, fill = OBESITY)) +
+  geom_bar() +
+  labs(
+    title = "Stacked Bar Chart of Obesity and Classification",
+    x = "CLASIFICATION_FINAL",
+    y = "count",
+    fill = "OBESITY"
+  ) +
+  theme_minimal()
 
 ggplot2::ggsave(
   fig3, 
-  file = here::here("output/fig1.png")
+  file = here::here("output/graphical_analysis_fig3.png")
 )
 
